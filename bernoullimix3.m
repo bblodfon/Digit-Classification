e53b507dae52f428334c13289775e4b268348c51
John Zobolas <bblodfon@users.noreply.github.com>
2017-04-21 01:40:02 +0300

    changed comments from greeklish to english

--- bernoullimix3.m
+++ bernoullimix3.m
@@ -17,43 +17,41 @@ load mnist_all.mat;
 testDataList = {test0; test1; test2; test3; test4; test5; test6; test7; test8; test9}; 
 trainDataList = {train0; train1; train2; train3; train4; train5; train6; train7; train8; train9};
 
-% K = Poses bernoulli na baloume! (the more the better)
+% K = how many bernoulli we test with (the more the better)
 KList = [1 2 4 8 16 32];
 
-% keep the results of the m(K,D) bernoulli parameters kai ton ek ton
-% proteron pithanotiton p(K)
+% keep the results of the m(K,D) bernoulli parameters and of the apriori probabilities p(K)
 % 6 values for K (1,2,4,8,16,32) and 10 digits to train (0-9)
 mcell = cell(6,10);
 pcell = cell(6,10);
 
-% BALE 1:6 !!!!!
 for numOfK = 1:6
 
 K = KList(numOfK);
 fprintf('\n%%%%%%%%%% TRAINING FOR K=%d %%%%%%%%%%\n',K);
 
 for digit=1:10
-% ousiastika kanoume train to psifio (digit-1)!!!
+% train each digit
 
 % x = data matrix
 x = double(trainDataList{digit});
 
-% make the pixels to 1 or 0
+% make the pixels equal to 1 or 0
 x(x < 3) = 0;
 x(x > 0) = 1;
 
-% N = numberOfData, D = 784 - diastiasi dedomenon
+% N = numberOfData, D = 784 - dimension of data
 [N D]=size(x);
 
-%ARXIKOPOIISEIS
-% p = pinakas ton ek ton proteron pithanotiton p(k)=1/K
+% Initializations
+% p = matrix with the a priori probabilities: p(k)=1/K
 p = (1/K).*ones(1,K);
 
-% M = pinakas bernoulli parametron: 0.4 < M(k,d) < 0.6
+% m = matrix with the bernoulli parameters: 0.4 < m(k,d) < 0.6
 a = 0.4; b = 0.6;
 m = a + (b-a).*rand(K,D);
 
-% z(n,k) = pinakas me tis ek ton usteron pithanotites 
+% z(n,k) = matrix with the posterior probabilities 
 z = zeros(N,K);
 
 %%% EM ALGORITHM %%%
@@ -62,8 +60,8 @@ tic
 while 1
    
    %%%              E step                 %%%
-   %%%         ipologizo ta z(n,k)         %%% 
-   %%% me kolpa gia arithmitiki eustatheia %%%
+   %%%         calculate z(n,k)            %%% 
+   %%%  using numerical stability methods  %%%
    
    %fprintf('E step\n');
    
@@ -73,7 +71,7 @@ while 1
    f = f - maxf'*ones(1,K);
    f = exp(f);
    
-   % pinakas athroismaton by grammes
+   % a matrix which has as elements the sums of every raw of the f matrix
    sumf = sum(f,2);
    
    for n=1:N
@@ -81,8 +79,8 @@ while 1
    end
 
    %%%             M step                  %%%
-   %%%         ipologizo ta m(k,d)         %%% 
-   %%% me kolpa gia arithmitiki eustatheia %%%
+   %%%         calculate m(k,d)            %%% 
+   %%%  using numerical stability methods  %%%
    
    %fprintf('M step\n');
    
@@ -92,12 +90,11 @@ while 1
    m = sum1./temp;
    p = sum2/N; 
    
-   % epeidi mpainei se logarithmo epeita:
+   % trick because later we need to use the log(m)
    m(m==0) = 1e-10;
    m(m>=1) = 1 - 1e-10;
    
-   %%% ELEGXOS SIGKLISIS     %%%
-   %%% me eustatheia kai edo %%%
+   %%% CONVERGENCE TEST %%%
    
    f2 = x*log(m)' + (1-x)*(log(1-m))';
    fL = f2 + ones(N,1)*log(p);
@@ -107,7 +104,7 @@ while 1
    
    L_new = sum(maxfL' + log(sum(fL,2)));
    
-   %fprintf('L function: %f kai diafora: %f\n', L_new, L_new - L_old);
+   %fprintf('L function: %f and difference: %f\n', L_new, L_new - L_old);
     
    if (abs(L_new - L_old) < 1e-5)
       break;
@@ -120,7 +117,7 @@ end
 time=toc;
 fprintf('\nTotal Execution Time (EM): %f for K=%d and training digit=%d\n', time, K, digit-1);
 
-% kratame tis bernoulli parametrous + tis ek ton proteron pithanotites
+% we keep the bernoulli parameters + the a priori probabilities
 mcell{numOfK,digit} = m;
 pcell{numOfK,digit} = p;
 
